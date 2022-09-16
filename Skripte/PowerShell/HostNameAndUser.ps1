843dec0025df7c48d36a9158f675f2bdc44160f4
m4dm4n <brainbug123@gmail.com>
2022-09-17 00:18:56 +0200

    Create HostNameAndUser.ps1

---
+++ Skripte/PowerShell/HostNameAndUser.ps1
@@ -0,0 +1,53 @@
+$ipAddress = Get-NetIPAddress | Where-Object { $_.IPaddress -like "192.168.*" -and $_.PrefixOrigin -notmatch "Manual"} | Select-Object -ExpandProperty IPaddress
+
+$networkPortion = $ipAddress.Split(".")[2]
+
+$hostPortion = $ipAddress.Split(".")[3]
+
+
+if ( $networkPortion -match 70 ) { 
+    
+    $labLocation = 'lb32'
+
+} elseif ( $networkPortion -match 80 ) {
+
+    $labLocation = 'lb31'
+
+}  else {
+
+    Write-Output "Nesto nije u redu"
+
+}
+ 
+
+if ( $hostPortion -In 10..22 ) { 
+    
+    $hostValue = '{0:d2}' -f ($hostPortion - 10)
+
+} elseif ( $hostPortion -In 50..62 ) {
+
+    $hostValue = '{0:d2}' -f ($hostPortion - 10)
+
+}  else {
+
+    Write-Output "Nesto nije u redu"
+
+}
+
+
+Rename-Computer -NewName WS"$hostValue$labLocation"
+
+
+
+# Username and Password
+$username = WS$hostValue
+$password = ConvertTo-SecureString $username -AsPlainText -Force  # Super strong plane text password here (yes this isn't secure at all)
+
+# Creating the user
+New-LocalUser -Name "$username" -Password $password -FullName "$username" -Description "$username"
+
+
+
+Add-LocalGroupMember -Groups Administrators -Member $username
+
+Restart-Computer
