b6bb8d57b0af166616acd3e431c1025d684e7b9b
m4dm4n <brainbug123@gmail.com>
2024-05-02 13:16:51 +0200

    Add VM test deploy script

---
+++ PC_KONFIGURACIJE/2024/Skripte/Deployment/VMTestDeployment/VMTestDeployment.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#Create SHARE
+#mkdir ~/share
+#mount -t cifs //192.168.100.5/WinImages $shareDir/share -o username=Administrator
+
+shareDir=/home/ubuntu
+
+#Restore GPT_TABLES
+sgdisk -l $shareDir/share/GPTTablice/FestoTablice/All_Partitions.gpt /dev/nvme0n1
+partprobe
+
+#Create Filesystems
+for i in {1,5}; do mkfs.vfat -F 32 /dev/nvme0n1p$i; done
+mkswap /dev/nvme0n1p2
+for i in {3,4}; do mkfs.ext4 -F /dev/nvme0n1p$i; done
+for i in {7,8}; do mkfs.ntfs -Q /dev/nvme0n1p$i; done
+
+#Install Partclone
+apt install partclone -y
+
+#Restore LinuxOS
+partclone.fat32 -r -s $shareDir/share/Images/NEW_Linux/efiLinux.pcl -O /dev/nvme0n1p1
+partclone.ext4 -r -s $shareDir/share/Images/NEW_Linux/rootLinux.pcl -O /dev/nvme0n1p3
+partclone.ext4 -r -s $shareDir/share/Images/NEW_Linux/homeLinux.pcl -O /dev/nvme0n1p4
+
+#Restore Windows OSes
+#WIN01
+partclone.fat32 -r -s $shareDir/share/Images/Windows/EFI_backups/efi5Backup.pcl -O /dev/nvme0n1p5
+partclone.ntfs -r -s $shareDir/share/Images/Windows/Win10_01_Partclone/win01NEW.pcl -O /dev/nvme0n1p7
+ntfsresize --force --force /dev/nvme0n1p7
+
+
+#Restore only Linux GPT table
+sgdisk -l $shareDir/share/GPTTablice/RacTablice/Linux_Partitions.gpt /dev/nvme0n1
+
+reboot
